8d305ff3b301de961feacb6ca1bb94e74a7f76ab
Nazeli Sayra Kurtoğlu <sayrayfer@gmail.com>
2025-11-09 16:59:55 -0500

    christmas and dance musics added.

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource christmasMusic;
+    public AudioSource danceMusic;
+
+    void Start()
+    {
+        christmasMusic.Play();
+    }
+
+    public void SwitchToDanceMusic()
+    {
+        Debug.Log("SwitchToDanceMusic is called!");
+
+        if (christmasMusic.isPlaying)
+            christmasMusic.Stop();
+
+        danceMusic.Play();
+    }
+}
