97b8cecdcfbdd14f8c4e87a25b174f808a3d4a36
karenlorena0922-hub <karen.lorena0922@gmail.com>
2025-11-10 12:35:54 -0500

    Deer to main Scene

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -5,18 +5,44 @@ public class MusicController : MonoBehaviour
     public AudioSource christmasMusic;
     public AudioSource danceMusic;
 
+    // Track whether we are currently in "dance mode"
+    private bool isDancing = false;
+
     void Start()
     {
-        christmasMusic.Play();
+        // Start with Christmas music playing (optional)
+        if (christmasMusic != null)
+        {
+            christmasMusic.Play();
+        }
     }
 
-    public void SwitchToDanceMusic()
+    // Call this from the SAME button as the animation
+    public void ToggleDanceMusic()
     {
-        Debug.Log("SwitchToDanceMusic is called!");
+        Debug.Log("ToggleDanceMusic is called!");
 
-        if (christmasMusic.isPlaying)
-            christmasMusic.Stop();
+        if (!isDancing)
+        {
+            // Go INTO dance mode
+            if (christmasMusic != null && christmasMusic.isPlaying)
+                christmasMusic.Stop();
 
-        danceMusic.Play();
+            if (danceMusic != null && !danceMusic.isPlaying)
+                danceMusic.Play();
+        }
+        else
+        {
+            // Go OUT of dance mode
+            if (danceMusic != null && danceMusic.isPlaying)
+                danceMusic.Stop();
+
+            // If you want to go back to Christmas music:
+            if (christmasMusic != null && !christmasMusic.isPlaying)
+                christmasMusic.Play();
+        }
+
+        // Flip state
+        isDancing = !isDancing;
     }
 }
